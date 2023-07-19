d164b22e18fd64e4ba9fdf01e390999ad5de9aae
sccs604@naver.com <sccs604@naver.com>
2023-07-19 18:11:30 +0900

    NotFoundPage Home으로 연결

--- vue-bulletin-board/src/views/NotFoundView.vue
+++ vue-bulletin-board/src/views/NotFoundView.vue
@@ -4,13 +4,20 @@
 		<h2>404 Not Found</h2>
 		<div class="text-muted">요청한 페이지를 찾을 수 없습니다.</div>
 		<div class="mt-4">
-			<!-- <RouterLink> -->
-			<button class="btn btn-primary">Home</button>
+			<!-- <RouterLink to=""> -->
+			<button class="btn btn-primary" @click="goHomePage">Home</button>
 			<!-- </RouterLink> -->
 		</div>
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goHomePage = () => {
+	router.push('/');
+};
+</script>
 
 <style lang="scss" scoped></style>
