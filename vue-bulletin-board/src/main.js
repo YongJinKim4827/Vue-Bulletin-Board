bd15a4b524b812f3e01033a85f4bbe392a96eeb5
sccs604@naver.com <sccs604@naver.com>
2023-07-19 14:28:48 +0900

    bootstrap 설정

--- vue-bulletin-board/src/main.js
+++ vue-bulletin-board/src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import { createApp } from 'vue';
 import App from './App.vue';
+import 'bootstrap/dist/js/bootstrap.js';
 
 createApp(App).mount('#app');
